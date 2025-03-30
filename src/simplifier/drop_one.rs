08e15cd3a7b6dbe31d4497e12f337ca3cba37a83
Nic McPhee <mcphee@morris.umn.edu>
2025-03-30 12:21:51 -0500

    Deal with simplifying an empty genome
    If none of the instructions in the `Plushy` genome actually _do_ anything, then simplification can get down to an empty genome and then panic because there's not an instruction to remove.
    
    I've dealt with this by checking for an empty genome in the simplification loop, but we should probably also address this in `drop_random_instruction` as well.
    
    I also added some comments about possibly moving some of the logic to `Plushy` and `TestResults`.

--- src/simplifier/drop_one.rs
+++ src/simplifier/drop_one.rs
@@ -1,5 +1,6 @@
 use ec_core::{
     individual::scorer::Scorer,
+    population::Population,
     test_results::{self, TestResults},
 };
 use ordered_float::OrderedFloat;
@@ -27,8 +28,11 @@ impl<S> DropOne<S> {
         }
     }
 
+    // TODO: Should this move to `Plushy`?
     fn drop_random_instruction<R: Rng>(&self, genome: &Plushy, rng: &mut R) -> Plushy {
         let mut genes = genome.get_genes();
+        // This panics if the range is empty, i.e., if `genes.len()` is 0.
+        // We might want to have this return a `Result` type.
         let index = rng.random_range(0..genes.len());
         let _ = genes.remove(index);
         Plushy::new(genes)
@@ -37,6 +41,7 @@ impl<S> DropOne<S> {
     // TODO: We could change this allow _improvements_, i.e., reductions in
     //   error as the result of simplication. That can happen, and this would
     //   reject anything like that.
+    // TODO: Should this move to `TestResults`?
     fn nearly_equal_scores(
         &self,
         first: &TestResults<test_results::Error<OrderedFloat<f64>>>,
@@ -58,6 +63,9 @@ where
     fn simplify_genome<R: Rng>(&self, mut genome: Plushy, rng: &mut R) -> Plushy {
         let original_score = self.scorer.score(&genome);
         for _ in 0..self.num_simplification_attempts {
+            if genome.get_genes().size() == 0 {
+                break;
+            }
             let possible_simplification = self.drop_random_instruction(&genome, rng);
             let new_score = self.scorer.score(&possible_simplification);
             if self.nearly_equal_scores(&original_score, &new_score) {
